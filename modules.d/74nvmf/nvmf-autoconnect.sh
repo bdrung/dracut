ee632d27787729119f9d44fd88516a79c0016f51
Benjamin Drung <benjamin.drung@canonical.com>
2026-09-14 14:33:01 +0200

    chore(nvmf): drop supporting nvmi-cli < 2.5
    The code to check the `nvme` command to support `--nbft` does not work
    with busybox:
    
    ```sh
    $ /usr/sbin/nvme connect-all --help 2>&1 | busybox sed -n '/[[:space:]]--nbft[[:space:]]/q1;$q0'
    sed: unsupported command ;
    ```
    
    Remove support for nvmi-cli < 2.5 because all containers have a recent
    enough version (except for opensuse where `nvme` is not installed):
    
    ```
    $ containers=(alpine:busybox-edge alpine:edge arch:latest azurelinux:latest centos:latest debian:latest debian:sid fedora:latest fedora:rawhide gentoo:amd64-openrc gentoo:latest opensuse:latest ubuntu:devel ubuntu:rolling void:latest)
    $ for container in "${containers[@]}"; do podman run -it --rm "ghcr.io/dracut-ng/$container" /usr/sbin/nvme connect-all --help | grep -q -- --nbft || echo "$container"; done
    Error: runc: runc create failed: unable to start container process: error during container init: exec: "/usr/sbin/nvme": stat /usr/sbin/nvme: no such file or directory: OCI runtime attempted to invoke a command that was not found
    opensuse:latest
    ```

--- modules.d/74nvmf/nvmf-autoconnect.sh
+++ modules.d/74nvmf/nvmf-autoconnect.sh
@@ -21,21 +21,13 @@ fi
 NVMF_HOSTNQN_OK=
 [ ! -f "/etc/nvme/hostnqn" ] || [ ! -f "/etc/nvme/hostid" ] || NVMF_HOSTNQN_OK=1
 
-# Only nvme-cli 2.5 or newer supports the options --nbft and --no-nbft
-# for the connect-all command.
-# Make sure we don't use unsupported options with earlier versions.
-NBFT_SUPPORTED=
-# shellcheck disable=SC2016
-/usr/sbin/nvme connect-all --help 2>&1 | sed -n '/[[:space:]]--nbft[[:space:]]/q1;$q0' \
-    || NBFT_SUPPORTED=1
-
 if [ -e /tmp/nvmf-fc-auto ] && [ "$NVMF_HOSTNQN_OK" ] \
     && [ -f /sys/class/fc/fc_udev_device/nvme_discovery ]; then
     # prio 1: cmdline override "rd.nvmf.discovery=fc,auto"
     echo add > /sys/class/fc/fc_udev_device/nvme_discovery
     exit 0
 fi
-if [ "$NBFT_SUPPORTED" ] && [ -e /tmp/valid_nbft_entry_found ]; then
+if [ -e /tmp/valid_nbft_entry_found ]; then
     # prio 2: NBFT
     /usr/sbin/nvme connect-all --nbft
     exit 0
@@ -44,11 +36,7 @@ if [ -f /etc/nvme/discovery.conf ] || [ -f /etc/nvme/config.json ] \
     && [ "$NVMF_HOSTNQN_OK" ]; then
     # prio 3: configuration from initrd and/or kernel command line
     # We can get here even if "rd.nvmf.nonbft" was given, thus use --no-nbft
-    if [ "$NBFT_SUPPORTED" ]; then
-        /usr/sbin/nvme connect-all --no-nbft
-    else
-        /usr/sbin/nvme connect-all
-    fi
+    /usr/sbin/nvme connect-all --no-nbft
     exit 0
 fi
 if [ "$NVMF_HOSTNQN_OK" ] \
